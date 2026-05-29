6977f1efbaca4499519ddac790fdfce4ad8c8ad9
Costi Cristaldi <ccristaldi95@gmail.com>
2026-05-29 11:46:13 -0300

    Add files via upload

---
+++ Python/Practica de algoritmo/Ejercicio1.py
@@ -0,0 +1,9 @@
+# 1.Solicitar al usuario la palabra
+
+palabra_ingresada=input("Por favor ingrese la palabra: ")
+
+# 2.Contar la cantidad de letras que tiene la palabra ingresada
+cantidad_letras=len(palabra_ingresada)
+
+# 3.Mostrarle al usuario cuantas letras tiene la palabra ingresada
+print("La palabra ingresada tiene ", cantidad_letras, "letras")
